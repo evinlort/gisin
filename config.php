eebddc305e11a1a32b80da5db9959f006140d68c
Evgeny Grebnev <evinlorth@gmail.com>
2021-04-12 22:37:37 +0300

    Set mysql port from env

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ require './vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$db = new mysqli('localhost', $_ENV['USERNAME'], $_ENV['PASSWORD'], 'memory_app');
+$db = new mysqli('127.0.0.1', $_ENV['USERNAME'], $_ENV['PASSWORD'], 'memory_app', $_ENV['PORT']);
 $query = 'select * from config';
 $res = $db->query($query);
 $row = $res->fetch_assoc();
